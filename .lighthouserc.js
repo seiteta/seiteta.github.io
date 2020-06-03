afc89286c5b9c5975962dff169b9a71b62f7b5b5
seiteta <seiteta@users.noreply.github.com>
2020-06-03 22:18:37 +0200

    Use json config file instead of js

--- .lighthouserc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  ci: {
-    collect: {
-      url: 'https://example.com/'
-      // staticDistDir: '/__w/seiteta.github.io/seiteta.github.io/_site',
-      //  chromePath: '/'
-    }
-  }
-}
