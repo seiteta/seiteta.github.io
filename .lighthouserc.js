ee73e7cae96957028e3b63faf394d9f30a213308
seiteta <seiteta@users.noreply.github.com>
2020-06-03 22:03:37 +0200

    Correct URL

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: 'https://f14e.fr'
+      url: 'https://f14e.fr/'
       // staticDistDir: '/__w/seiteta.github.io/seiteta.github.io/_site',
       //  chromePath: '/'
     }
