1f40752ad13b751c1bfdfd5a5a733087ab35a5f6
seiteta <seiteta@users.noreply.github.com>
2020-06-09 14:15:00 +0200

    Add a lot of assertions 😍

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -5,8 +5,14 @@ module.exports = {
     },
     assert: {
       assertions: {
+        "categories:performance": ["warn", {"minScore": 1}],
+        "categories:performance": ["error", {"minScore": 0.8}],
         "categories:accessibility": ["warn", {"minScore": 1}],
-        "categories:accessibility": ["error", {"minScore": 0.8}]
+        "categories:accessibility": ["error", {"minScore": 0.8}],
+        "categories:best-practices": ["warn", {"minScore": 1}],
+        "categories:best-practices": ["error", {"minScore": 0.8}],
+        "categories:seo": ["warn", {"minScore": 1}],
+        "categories:seo": ["error", {"minScore": 0.8}],
       },
     },
   },
