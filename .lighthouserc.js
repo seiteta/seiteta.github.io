d5f11f5e5a54b1ae2d8d916178586bbf6834cc57
seiteta <seiteta@users.noreply.github.com>
2020-06-09 14:07:02 +0200

    Add an assertion

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,6 +2,12 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: ['/home/runner/work/seiteta.github.io/seiteta.github.io']
-    }
-  }
-}
+    },
+    assert: {
+      assertions: {
+        "categories:accessibility": ["warn", {"minScore": 1}],
+        "categories:accessibility": ["error", {"minScore": 0.8}]
+      },
+    },
+  },
+};
