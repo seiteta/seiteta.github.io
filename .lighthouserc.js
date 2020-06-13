ebd68df70d04b6ff9dc7d399c80db70df4308a2b
seiteta <seiteta@users.noreply.github.com>
2020-06-13 15:07:34 +0200

    Assert with LHCI

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,19 +2,19 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: '/home/runner/work/seiteta.github.io/seiteta.github.io',
-      url: ['http://localhost/site/index.html'],
+      url: ['http://localhost/site/index.html', 'http://localhost/site/hackerspublics/index.html'],
       numberOfRuns: 1
     },
     assert: {
       assertions: {
         "categories:performance": ["warn", {"minScore": 1}],
-        "categories:performance": ["error", {"minScore": 0.8}],
+        "categories:performance": ["error", {"minScore": 0.9}],
         "categories:accessibility": ["warn", {"minScore": 1}],
-        "categories:accessibility": ["error", {"minScore": 0.8}],
+        "categories:accessibility": ["error", {"minScore": 0.9}],
         "categories:best-practices": ["warn", {"minScore": 1}],
-        "categories:best-practices": ["error", {"minScore": 0.7}],
+        "categories:best-practices": ["error", {"minScore": 0.9}],
         "categories:seo": ["warn", {"minScore": 1}],
-        "categories:seo": ["error", {"minScore": 0.8}],
+        "categories:seo": ["error", {"minScore": 0.9}],
       },
     },
     upload: {
