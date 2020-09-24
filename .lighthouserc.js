3e7a980e29e6a7023f0ca5b6bd0629468ee0a239
Frédéric Bardolle <seiteta@users.noreply.github.com>
2020-09-24 14:49:31 +0200

    Update .lighthouserc.js

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -8,7 +8,7 @@ module.exports = {
     assert: {
       assertions: {
         "categories:performance": ["warn", {"minScore": 1}],
-        "categories:performance": ["error", {"minScore": 0.77}],
+        "categories:performance": ["error", {"minScore": 0.7}],
         "categories:accessibility": ["warn", {"minScore": 1}],
         "categories:accessibility": ["error", {"minScore": 0.9}],
         "categories:best-practices": ["warn", {"minScore": 1}],
